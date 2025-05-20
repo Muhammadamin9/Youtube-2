d9104bb015d299ab0852f8149e010e0f3fd6a92c
Muhammadamin9 <amin4ik.angel@gmail.com>
2025-05-20 12:32:28 +0500

    more style added

---
+++ js/index.js
@@ -0,0 +1,5 @@
+let logoButton = document.querySelector('.header__icon');
+let profileIcon = document.querySelector('.header__notifications-button');
+logoButton.addEventListener('click',()=>{
+    location.reload();
+});
