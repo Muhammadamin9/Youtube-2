6183614986a729d5e34505b60537caa4914a759d
Muhammadamin9 <amin4ik.angel@gmail.com>
2025-05-21 16:24:57 +0500

    f

--- js/index.js
+++ js/index.js
@@ -1,14 +1,4 @@
-const wrapper = document.querySelector(".wrapper");
-
-videoData.forEach((el) => {
-  wrapper.insertAdjacentHTML(
-    "beforeend",
-    ` 
-      <h1>${el.title}</h1>
-        <img src="${el.thumbnail}" alt="" />
-  
-      <p>${el.channel}</p>
-      <img src="${el.channelPhoto}" alt="" />
-    `
-  );
-});
+let youtubeLogo = document.querySelector('.header__icon');
+youtubeLogo.addEventListener('click',function(){
+  location.reload();
+})
