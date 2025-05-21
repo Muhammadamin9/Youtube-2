0f9c6308a235469c5dab0f5b4d12818763e8ff42
Muhammadamin9 <amin4ik.angel@gmail.com>
2025-05-21 17:14:38 +0500

    f

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
 let youtubeLogo = document.querySelector('.header__icon');
 youtubeLogo.addEventListener('click',function(){
   location.reload();
-})
+});
+
+let videosUl = document.querySelector('.main-list__item');
+let searchInput = document.querySelector('.header__input');
+searchInput.addEventListener('input',function(){
+  let filteredVideo = videoData.filter((el)=>el.title.toLowerCase().includes(searchInput.value.toLowerCase()));
+});
