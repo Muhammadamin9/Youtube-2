9e55a42a1edbc76546b2a6c26666a39796adccaa
Nematjons <154860161+Nematjons@users.noreply.github.com>
2025-05-27 14:29:23 +0500

    f

--- js/detail.js
+++ js/detail.js
@@ -3,3 +3,9 @@ let recomendVideos = document.querySelector("detail__recomend-videos");
 
 let params = new URLSearchParams(window.location.search);
 let id = params.get("id");
+let find = products.find((el) => el.id === id);
+detailWrapper.innerHTML = `
+ <div class="detail__video">
+        <iframe class="detail__video-screen" src="https://www.youtube.com/embed/8RBm0qKOmHY" frameborder="0"></iframe>
+     </div>
+`
